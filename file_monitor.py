fd06aef56e203d1580ef40bf9f9eb3f169c0d597
Martijn Hemeryck <martijn.hemeryck@gmail.com>
2024-05-31 20:34:08 +0200

    some experiment

--- file_monitor.py
+++ file_monitor.py
@@ -7,11 +7,49 @@ FILENAME: str = "relay_state"
 INTERVAL = 0.100
 
 
-async def file_monitor(filename: str, interval: float = INTERVAL) -> typing.AsyncIterator[bool]:
+# class FileMonitor(collections.abc.AsyncGenerator):
+#     def __init__(self, filename: str, interval: float) -> None:
+#         self._filename = filename
+#         self._interval = interval
+#         self._state = False
+#         self._previous = False
+
+#     async def __aiter__(self) -> typing.Self:
+#         return self
+
+#     async def __anext__(self) -> bool:
+#         async with aiofiles.open(self._filename, "r") as fh:
+#             new = await fh.read()
+#             match new:
+#                 case "1\n":
+#                     self._state = True
+#                 case "0\n":
+#                     self._state = False
+#                 case _:
+#                     print(new)
+#                     pass
+#             if self._state != self._previous:
+#                 print(f"Found something new {self._previous} -> {self._state}")
+#                 self._state = self._previous
+#                 return self._state
+
+#     async def asend(self, state: bool) -> None:
+#         async with aiofiles.open(self._filename, mode="w") as fh:
+#             data = "1" if state else "0"
+#             data += "\n"
+#             await fh.write(data)
+#             await fh.flush()
+
+#     async def athrow(self, typ, val=None, tb=None):
+#         self._exception = typ(val)
+
+
+async def file_monitor(filename: str, interval: float = INTERVAL) -> typing.AsyncGenerator[bool, bool]:
     state = False
     old = False
     while True:
-        async with aiofiles.open(filename, "r") as fh:
+        async with aiofiles.open(filename, "r+") as fh:
+            # read
             new = await fh.read()
             match new:
                 case "1\n":
@@ -26,11 +64,31 @@ async def file_monitor(filename: str, interval: float = INTERVAL) -> typing.Asyn
                 old = state
                 yield state
             await asyncio.sleep(interval)
+            # write, if needed
+            update = yield update
+            print(f"got value update: {update}")
+            if update != state:
+                await fh.seek(0)
+                await fh.write("1\n" if update else "0\n")
+                await fh.flush()
+                state = update
 
 
 async def main() -> None:
-    async for event in file_monitor(FILENAME):
-        print(event)
+    fm = file_monitor(FILENAME)
+    # async for event in file_monitor(FILENAME):
+    #     print(event)
+    # file_monitor = FileMonitor(FILENAME, INTERVAL)
+
+    async def reader():
+        async for event in fm:
+            print(f"Read an event: {event}")
+
+    async def writer():
+        await fm.asend(True)
+        # await fm.asend(False)
+
+    asyncio.gather(reader(), writer())
 
 
 if __name__ == "__main__":
