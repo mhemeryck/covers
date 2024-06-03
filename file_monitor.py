206b36d2a6aa8ec9497afc4c84366423b16ad009
Martijn Hemeryck <martijn.hemeryck@gmail.com>
2024-06-03 22:18:03 +0200

    more tests

--- file_monitor.py
+++ file_monitor.py
@@ -21,13 +21,17 @@ _event_q_type = asyncio.Queue[typing.Tuple[str, str]]
 
 
 async def worker(file_handle_q: _file_handle_q_type, event_q: _event_q_type) -> None:
-    fid, fh = await file_handle_q.get()
-    try:
-        await fh.seek(0)
-        data = await fh.read()
-    finally:
-        file_handle_q.task_done()
-    await event_q.put((fid, data))
+    logger.debug("start worker")
+    while True:
+        fid, fh = await file_handle_q.get()
+        try:
+            await fh.seek(0)
+            data = await fh.read()
+            # if fid.endswith("relay_state_1"):
+            #     logger.debug(f"worker checking {fid} - {data}")
+        finally:
+            file_handle_q.task_done()
+        await event_q.put((fid, data))
 
 
 class FileMonitor:
@@ -51,7 +55,7 @@ class FileMonitor:
             self._file_handle = await aiofiles.open(self._filename, "r+")
         return self._file_handle
 
-    async def poll(self, interval: float = INTERVAL) -> None:
+    async def submit(self, interval: float = INTERVAL) -> None:
         """Poll produces polling jobs"""
         while True:
             async with self._file_lock:
@@ -59,33 +63,34 @@ class FileMonitor:
                 await self._file_handle_q.put((self._id, fh))
             await asyncio.sleep(interval)
 
-    async def read(self, interval: float = INTERVAL) -> typing.AsyncGenerator[bool, None]:
-        """read consumes events coming back from the polling jobs"""
-        while True:
-            # async with self._file_lock:
-            #     fh = await self._get_file_handle()
-            #     await fh.seek(0)
-            #     new = await fh.read()
-            (fid, new) = await self._event_q.get()
-            # Ignore events which aren't ours
-            if fid != self._id:
-                continue
-            self._event_q.task_done()
+    # async def poll(self, interval: float = INTERVAL) -> typing.AsyncGenerator[bool, None]:
+    #     """read consumes events coming back from the polling jobs"""
+    #     while True:
+    #         # async with self._file_lock:
+    #         #     fh = await self._get_file_handle()
+    #         #     await fh.seek(0)
+    #         #     new = await fh.read()
+    #         (fid, new) = await self._event_q.get()
+    #         # Ignore events which aren't ours
+    #         if fid != self._id:
+    #             logger.debug(f"mismatching id {fid} != {self._id}")
+    #             continue
+    #         self._event_q.task_done()
 
-            match new:
-                case "1\n":
-                    self._state = True
-                case "0\n":
-                    self._state = False
-                case _:
-                    logger.debug(f"something went wrong, state: {new}")
-                    pass
+    #         match new:
+    #             case "1\n":
+    #                 self._state = True
+    #             case "0\n":
+    #                 self._state = False
+    #             case _:
+    #                 logger.debug(f"something went wrong, state: {new}")
+    #                 pass
 
-            if self._state != self._previous:
-                logger.debug(f"Found something new {self._previous} -> {self._state}")
-                self._previous = self._state
-                yield self._state
-            await asyncio.sleep(interval)
+    #         if self._state != self._previous:
+    #             logger.debug(f"Found something new {self._previous} -> {self._state}")
+    #             self._previous = self._state
+    #             yield self._state
+    #         await asyncio.sleep(interval)
 
     async def write(self, state: bool) -> int:
         logger.debug("trigger write")
@@ -98,6 +103,38 @@ class FileMonitor:
         return n
 
 
+async def process(queue: _event_q_type) -> typing.AsyncGenerator[bool, None]:
+    states: typing.Dict[str, bool] = {}
+    while True:
+        (fid, new) = await queue.get()
+        queue.task_done()
+
+        if fid.endswith("relay_state_1"):
+            logger.debug(f"{fid} - {new}")
+
+        yield new == "1\n"
+
+        # match new:
+        #     case "1\n":
+        #         state = True
+        #     case "0\n":
+        #         state = False
+        #     case _:
+        #         logger.debug(f"something went wrong, state: {new}")
+        #         pass
+
+        # try:
+        #     previous = states[fid]
+        # except KeyError:
+        #     states[fid] = previous = not state
+
+        # if state != previous:
+        #     logger.debug(f"Found something new {fid}: {previous} -> {state}")
+        #     states[fid] = state
+        #     yield state
+        await asyncio.sleep(INTERVAL)
+
+
 async def main() -> None:
     paths = []
     for root, _, files in os.walk("./fixtures"):
@@ -105,27 +142,24 @@ async def main() -> None:
             if "relay_state_" in f:
                 paths.append(os.path.join(root, f))
 
-    monitors = [FileMonitor(p) for p in paths]
-    # fm = FileMonitor(FILENAME)
+    file_handle_q = asyncio.Queue()
+    event_q = asyncio.Queue()
 
-    # async def reader():
-    #     async for event in fm.read():
-    #         logger.debug(f"Read an event: {event}")
+    monitors = [FileMonitor(p, file_handle_q, event_q) for p in paths]
 
-    # async def writer():
-    #     await asyncio.sleep(2)
-    #     await fm.write(True)
-    #     await asyncio.sleep(2)
-    #     await fm.write(False)
-    #     await asyncio.sleep(2)
+    # async def reader(monitor: FileMonitor) -> None:
+    #     async for event in monitor.poll():
+    #         logger.debug(f"Read event for monitor {monitor._filename}: {event}")
+    async def reader() -> None:
+        logger.debug("process")
+        async for event in process(event_q):
+            logger.debug(f"process: {event}")
 
-    # await asyncio.gather(reader(), writer())
-
-    async def reader(monitor: FileMonitor) -> None:
-        async for event in monitor.read():
-            logger.debug(f"Read event for monitor {monitor._filename}: {event}")
-
-    await asyncio.gather(*[reader(monitor) for monitor in monitors])
+    producers = [monitor.submit() for monitor in monitors]
+    # producers += [reader()]
+    workers = [worker(file_handle_q, event_q) for _ in range(10)]
+    jobs = producers + workers + [reader()]
+    await asyncio.gather(*jobs)
 
 
 if __name__ == "__main__":
