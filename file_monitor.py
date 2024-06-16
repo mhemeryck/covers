d5fd08d9b0490bddae95de59d95731b7fbf22371
Martijn Hemeryck <martijn.hemeryck@gmail.com>
2024-06-16 18:01:03 +0200

    fixing the issue with a global watcher

--- file_monitor.py
+++ file_monitor.py
@@ -1,12 +1,11 @@
 import asyncio
-import aiofiles
 import os
 import re
 import typing
-from aiofiles.threadpool.text import AsyncTextIOWrapper
 
-
+import aiofiles
 import watchfiles
+from aiofiles.threadpool.text import AsyncTextIOWrapper
 
 WATCH_DIRECTORY = "./fixtures"
 
@@ -20,15 +19,29 @@ def crawl(folder: str) -> typing.List[str]:
     for root, _, files in os.walk(folder):
         for f in files:
             filename = os.path.join(root, f)
-            filename = os.path.normpath(filename)
+            filename = os.path.abspath(filename)
             if _FILENAME_PATTERN.match(filename) is not None:
                 result.append(filename)
     return result
 
 
-def only_modified(change: watchfiles.Change, path: str) -> bool:
-    print(change, path)
-    return change == watchfiles.Change.modified
+def file_filter(change: watchfiles.Change, path: str) -> bool:
+    return change == watchfiles.Change.modified and any(
+        path.endswith(f"{ending}_value") for ending in ("di", "do", "ro")
+    )
+
+
+async def watcher() -> typing.AsyncGenerator[bool, None]:
+    async for event in watchfiles.awatch(WATCH_DIRECTORY, force_polling=True, watch_filter=file_filter):
+        print(event)
+        for _, filename in tuple(event):
+            # ((_, filename),) = tuple(event)
+            print(filename)
+            device = devices()[os.path.abspath(filename)]
+            print(device, device._state)
+            # yield await self._read_state()
+            new = await device._read_state()
+            print(device, new, device._state)
 
 
 class Device:
@@ -60,16 +73,26 @@ class Device:
             await fh.seek(0)
             n = await fh.write(payload)
             print("written", n, payload)
-        # await self._read_state()
+        await self._read_state()
         print("finished writing state", state)
 
-    async def read(self) -> typing.AsyncGenerator[bool, None]:
-        async for _ in watchfiles.awatch(self._filename, force_polling=True):
-            print("events", self._filename)
-            yield await self._read_state()
+    # async def read(self) -> typing.AsyncGenerator[bool, None]:
+    #     async for _ in watchfiles.awatch(self._filename, force_polling=True):
+    #         print("events", self._filename)
+    #         yield await self._read_state()
 
     async def write(self, state: bool) -> None:
         await self._write_state(state)
+
+
+_DEVICES: typing.Dict[str, Device] = {}
+
+
+def devices() -> typing.Dict[str, Device]:
+    global _DEVICES
+    if not _DEVICES:
+        _DEVICES = {filename: Device(filename) for filename in crawl(WATCH_DIRECTORY)}
+    return _DEVICES
 
 
 async def writer(device) -> None:
@@ -82,6 +105,10 @@ async def writer(device) -> None:
     print("trigger to false")
     print("device state", device._state)
     await device.write(False)
+    print("sleeping")
+    await asyncio.sleep(2)
+    print("trigger to true")
+    await device.write(True)
 
 
 async def reader(device) -> None:
@@ -93,8 +120,9 @@ async def main() -> None:
     filenames = crawl(WATCH_DIRECTORY)
     jobs = []
     for filename in filenames:
-        device = Device(filename)
-        jobs += [writer(device), reader(device)]
+        device = devices()[filename]
+        jobs += [writer(device)]
+    jobs.append(watcher())
     await asyncio.gather(*jobs)
 
 
