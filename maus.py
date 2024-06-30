59e4e61c16107950c77f9d754d4837eeac757345
Martijn Hemeryck <martijn.hemeryck@gmail.com>
2024-06-30 17:48:30 +0200

    work-in-progress

--- maus.py
+++ maus.py
@@ -3,7 +3,12 @@ import asyncio
 import dataclasses
 import enum
 import logging
+import pathlib
 import typing
+
+import aiofiles
+import watchfiles
+from aiofiles.threadpool.text import AsyncTextIOWrapper
 
 # TODO: get config from CLI
 WATCH_DIRECTORY = "./fixtures"
@@ -49,25 +54,73 @@ class HasIdentifier(typing.Protocol):
         """Can generate an identifier"""
 
 
-@dataclasses.dataclass
 class IO(EventHandler, HasIdentifier):
+    PAYLOAD_ON = "1"
+    PAYLOAD_OFF = "0"
+
     queue: asyncio.Queue
     name: str
     state: bool
+
+    def __init__(self, queue: asyncio.Queue[Event], filename: str, state: bool) -> None:
+        self._queue = queue
+        self._filename = filename
+        self._state = state
+
+        self._state_lock = asyncio.Lock()
+        self._read_file_handle = None
+        self._write_file_handle = None
 
     async def handle(self, event: Event) -> None:
         match event:
             case Event(Identifier(_, EventType.LIGHT, _), payload):
                 self.state = payload
-                # At this point we probably also need to write something back ...
-                await self.queue.put(Event(self.identifier(), payload))
                 logger.debug("writing to do something to the light!")
+                await self.write(payload)
+                await self._queue.put(Event(self.identifier(), payload))
             case _:
                 logger.warning("%s can't handle event %s", self, event)
 
     def identifier(self) -> Identifier:
         # TODO: fix device identifier
         return Identifier("shady", self.name, EventType.IO)
+
+    async def _get_write_file_handle(self) -> AsyncTextIOWrapper:
+        if self._write_file_handle is None:
+            self._write_file_handle = await aiofiles.open(self._filename, "w")
+        return self._write_file_handle
+
+    async def _get_read_file_handle(self) -> AsyncTextIOWrapper:
+        if self._read_file_handle is None:
+            self._read_file_handle = await aiofiles.open(self._filename, "r")
+        return self._read_file_handle
+
+    async def read(self) -> bool:
+        async with self._state_lock:
+            logger.debug("reading %s", self)
+            fh = await self._get_read_file_handle()
+            await fh.seek(0)
+            data = await fh.read(1)
+            logger.debug(data)
+            match data:
+                case IO.PAYLOAD_ON:
+                    self._state = True
+                case IO.PAYLOAD_OFF:
+                    self._state = False
+                case _:
+                    logger.warning("Could not match state: %s", data)
+            return self._state
+
+    async def write(self, state: bool) -> None:
+        payload = IO.PAYLOAD_ON if state else IO.PAYLOAD_OFF
+        logger.debug(payload)
+        async with self._state_lock:
+            logger.debug("writing %s", self)
+            fh = await self._get_write_file_handle()
+            await fh.seek(0)
+            await fh.write(f"{payload}\n")
+            # await fh.writelines([payload])
+            await fh.flush()
 
 
 @dataclasses.dataclass
@@ -108,6 +161,31 @@ class Light(EventHandler, HasIdentifier):
 
 Entity = PushButton | Light
 Entry = IO | Entity
+
+
+class FileMonitorMixin:
+    """Functionality specifically for SysFS integration"""
+
+
+    def _crawl(self, folder: pathlib.Path)
+
+    @staticmethod
+    def device_filter(change: watchfiles.Change, path: str) -> bool:
+        return change == watchfiles.Change.modified and any(
+            path.endswith(f"{ending}_value") for ending in ("di", "do", "ro")
+        )
+
+    async def read(self, folder: pathlib.Path) -> typing.AsyncGenerator[Event, None]:
+        async for event in watchfiles.awatch(folder, force_polling=True, watch_filter=FileMonitorMixin.device_filter):
+            logger.debug(event)
+            for _, filename in tuple(event):
+                # logger.debug(filename)
+                device = self._devices_for_filename[os.path.abspath(filename)]
+                io: IO
+                # logger.debug(device)
+                state = await device.read()
+                # logger.debug(device)
+                yield Event(io.identifier(), state)
 
 
 class Maus:
